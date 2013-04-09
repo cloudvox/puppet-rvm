434117a0d69b8cfe522003219a2f73b194fc03b4
John Arundel <john@bitfieldconsulting.com>
2013-04-09 16:38:57 +0100

    Move stage definition into site.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,4 @@
 class rvm($version='latest', $install_rvm=true) {
-  stage { 'rvm-install': before => Stage['main'] }
-
   if $install_rvm {
     class {
       'rvm::dependencies': stage => 'rvm-install';
